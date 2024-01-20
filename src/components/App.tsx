f9737c1960a0e0fe39d7ba93b55fbbdce28b63d3
Mikuno Yasuhiro <396dance@gmail.com>
2024-01-20 22:01:08 +0900

    実装完了

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
-import React, { ChangeEvent, FC, useState } from "react";
+import React, { ChangeEvent, FC, useCallback, useState } from "react";
 import styled from "styled-components";
+import { MemoList } from "./MemoList.tsx";
+import { useMemoList } from "../hooks/useMemoList.ts";
 
 export const App: FC = () => {
+  // カスタムフックからそれぞれ取得
+  const { memos, addTodo, deleteTodo } = useMemoList();
   //　テキストボックスState
   const [text, setText] = useState<string>("");
 
   //　メモ一覧State
-  const [memos, setMemos] = useState<string[]>([]);
+  // const [memos, setMemos] = useState<string[]>([]);
 
   // テキストボックス入力時に入力内容をStateに設定
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
@@ -15,29 +19,40 @@ export const App: FC = () => {
   // 追加ボタン押下時
   const onClickAdd = () => {
     // State変更を正常に検知させるため新たな配列を生成
-    const newMemos = [...memos];
+    // const newMemos = [...memos];
     //  テキストボックスの入力内容をメモ配列に追加
-    newMemos.push(text);
-    setMemos(newMemos);
+    // newMemos.push(text);
+    // setMemos(newMemos);
+    //　カスタムフックのロジック実行
+    addTodo(text);
     // 　テキストボックスを空にする
-    setText("");
+    // setText("");
   };
 
   // 削除ボタン押下時（何番目を謳歌されたかを引数で受け取る）
-  const onClickDelete = (index: number) => {
-    // State変更を正常に検知させるため新たな配列を生成
-    const newMemos = [...memos];
-    // メモ配列から該当の要素を削除
-    newMemos.splice(index, 1);
-    setMemos(newMemos);
-  };
+  // const onClickDelete = (index: number) => {
+  const onClickDelete = useCallback(
+    (index: number) => {
+      // State変更を正常に検知させるため新たな配列を生成
+      // const newMemos = [...memos];
+      // メモ配列から該当の要素を削除
+      // newMemos.splice(index, 1);
+      // setMemos(newMemos);
+      // };
+      // },[memos]);
+      // カスタムフックのメモ削除ロジック実行
+      deleteTodo(index);
+    },
+    [deleteTodo]
+  );
 
   return (
     <div>
       <h1>簡単メモアプリ</h1>
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
-      <SContainer>
+      <MemoList memos={memos} onClickDelete={onClickDelete} />
+      {/* <SContainer>
         <p>メモ一覧</p>
         <ul>
           {memos.map((memo, index) => (
@@ -49,7 +64,7 @@ export const App: FC = () => {
             </li>
           ))}
         </ul>
-      </SContainer>
+      </SContainer> */}
     </div>
   );
 };
@@ -58,13 +73,13 @@ const SButton = styled.button`
   mergin-left: 16px;
 `;
 
-const SContainer = styled.div`
-  border: solid 1px #ccc;
-  padding: 16px;
-  margin: 8px;
-`;
+// const SContainer = styled.div`
+//   border: solid 1px #ccc;
+//   padding: 16px;
+//   margin: 8px;
+// `;
 
-const SMemoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
+// const SMemoWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+// `;
