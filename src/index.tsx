7093146a31c74194f228f822b52a734814f116c7
Mikuno Yasuhiro <396dance@gmail.com>
2024-01-20 15:19:49 +0900

    メモアプリ実装

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+import React from "react";
+import { App } from "./components/App.tsx";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
