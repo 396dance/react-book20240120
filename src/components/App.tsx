d80d53ad51044127f38f61e6034539db259b593d
Mikuno Yasuhiro <396dance@gmail.com>
2024-01-21 12:04:51 +0900

    コード整理

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,37 +9,19 @@ export const App: FC = () => {
   //　テキストボックスState
   const [text, setText] = useState<string>("");
 
-  //　メモ一覧State
-  // const [memos, setMemos] = useState<string[]>([]);
-
   // テキストボックス入力時に入力内容をStateに設定
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
     setText(e.target.value);
 
   // 追加ボタン押下時
   const onClickAdd = () => {
-    // State変更を正常に検知させるため新たな配列を生成
-    // const newMemos = [...memos];
-    //  テキストボックスの入力内容をメモ配列に追加
-    // newMemos.push(text);
-    // setMemos(newMemos);
     //　カスタムフックのロジック実行
     addTodo(text);
-    // 　テキストボックスを空にする
-    // setText("");
   };
 
-  // 削除ボタン押下時（何番目を謳歌されたかを引数で受け取る）
-  // const onClickDelete = (index: number) => {
+  // 削除ボタン押下時（何番目を押下されたかを引数で受け取る）
   const onClickDelete = useCallback(
     (index: number) => {
-      // State変更を正常に検知させるため新たな配列を生成
-      // const newMemos = [...memos];
-      // メモ配列から該当の要素を削除
-      // newMemos.splice(index, 1);
-      // setMemos(newMemos);
-      // };
-      // },[memos]);
       // カスタムフックのメモ削除ロジック実行
       deleteTodo(index);
     },
@@ -52,19 +34,6 @@ export const App: FC = () => {
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
       <MemoList memos={memos} onClickDelete={onClickDelete} />
-      {/* <SContainer>
-        <p>メモ一覧</p>
-        <ul>
-          {memos.map((memo, index) => (
-            <li key={memo}>
-              <SMemoWrapper>
-                <p>{memo}</p>
-                <SButton onClick={() => onClickDelete(index)}>削除</SButton>
-              </SMemoWrapper>
-            </li>
-          ))}
-        </ul>
-      </SContainer> */}
     </div>
   );
 };
@@ -72,14 +41,3 @@ export const App: FC = () => {
 const SButton = styled.button`
   mergin-left: 16px;
 `;
-
-// const SContainer = styled.div`
-//   border: solid 1px #ccc;
-//   padding: 16px;
-//   margin: 8px;
-// `;
-
-// const SMemoWrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-// `;
